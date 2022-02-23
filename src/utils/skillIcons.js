82042f7870953ceee6810e67290810bd8a8c9393
pimoux <luka.vegeta@laposte.net>
2022-02-23 16:48:46 +0100

    changed size of skill icons

--- src/utils/skillIcons.js
+++ src/utils/skillIcons.js
@@ -3,37 +3,37 @@ import { faCss3Alt, faDocker, faJs, faNodeJs, faPhp, faReact, faSymfony } from "
 const skills = [
     {
         icon: faCss3Alt,
-        className: "text-indigo-500 px-8 skill-icon",
+        className: "text-indigo-500 px-8 py-4 skill-icon",
         rate: 8
     },
     {
         icon: faJs,
-        className: "text-yellow-500 px-8 skill-icon",
+        className: "text-yellow-500 px-8 py-4 skill-icon",
         rate: 8
     },
     {
         icon: faPhp,
-        className: "text-indigo-500 px-8 skill-icon",
+        className: "text-indigo-500 px-8 py-4 skill-icon",
         rate: 8
     },
     {
         icon: faReact,
-        className: "text-turquoise px-8 skill-icon",
+        className: "text-turquoise px-8 py-4 skill-icon",
         rate: 9
     },
     {
         icon: faSymfony,
-        className: "px-8 skill-icon",
+        className: "px-8 py-4 skill-icon",
         rate: 8
     },
     {
         icon: faNodeJs,
-        className: "text-green-500 px-8 skill-icon",
+        className: "text-green-500 px-8 py-4 skill-icon",
         rate: 7
     },
     {
         icon: faDocker,
-        className: "text-turquoise px-8 skill-icon",
+        className: "text-turquoise px-8 py-4 skill-icon",
         rate: 5
     },
 ]
